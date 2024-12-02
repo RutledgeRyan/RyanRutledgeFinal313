5c035f5ab83d2640a4c220315448f97f22ac8481
RutledgeRyan <123197385+RutledgeRyan@users.noreply.github.com>
2024-12-01 21:53:18 -0500

    Add files via upload

--- ryanrutledgefinal/src/App.js
+++ ryanrutledgefinal/src/App.js
@@ -1,24 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
-import CardContainer from './components/CardContainer';
 import './App.css';
 
 function App() {
   const headingTitle = "Ryan Rutledge, Website and Software Designer";
-  const [portfolio, setPortfolio] = useState([]);
+  
 
-  useEffect(() => {
-    // Fetch data from the API
-    fetch('http://localhost:5000/api/project')
-      .then(response => response.json())
-      .then(data => setPortfolio(data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
-
-  const handleCardClick = (portfolio) => {
-    console.log('Card clicked:', portfolio);
-  };
+ 
 
   return (
     <div className="App">
